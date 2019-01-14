647d4fabb6c1cefa306499fb0edbdabe9d26ccd8
james Jarvis <jamespjarvis@gmail.com>
2019-01-14 05:03:16 -0500

    workbox options

--- app/vue.config.js
+++ app/vue.config.js
@@ -28,13 +28,15 @@ module.exports = {
     msTileColor: "#FFFFFF",
     appleMobileWebAppCapable: "yes",
     appleMobileWebAppStatusBarStyle: "black",
-    skipWaiting: true,
     iconPaths: {
       favicon32: "img/icons/favicon-32x32.png",
       favicon16: "img/icons/favicon-16x16.png",
       appleTouchIcon: "img/icons/apple-touch-icon-152x152.png",
       maskIcon: "img/icons/safari-pinned-tab.svg",
       msTileImage: "img/icons/msapplication-icon-144x144.png"
+    },
+    workboxOptions: {
+      skipWaiting: true
     }
   }
 };
